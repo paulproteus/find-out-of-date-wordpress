fb664c9d20667aa9d7ed98ca5b07bc844392b67a
Asheesh Laroia <paulproteus@rose.makesad.us>
2010-01-01 20:31:00 -0500

    Initial sketch up

---
+++ look.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+## Strategy:
+
+# use "locate" to find readme.html files since WordPress bundles one
+# Use a heuristic to check that it's the WordPress readme.html
+# If so, use a heuristic to get its version number
+
+function find_readme_html_files() {
+#    locate readme.html
+    echo /home/skyfaller/web/wrongsideofdawn.com/wordpress/readme.html
+    echo /home/skyfaller/web/wrongsideofdawn.com/wordpress/license.txt
+}
+
+function looks_like_wordpress_readme_html() {
+    grep -qi wordpress.*readme "$1"
+}
+    
+
+for file in $(find_readme_html_files); do
+    if looks_like_wordpress_readme_html "$file"; then
+        echo "$file"
+    fi
+done
